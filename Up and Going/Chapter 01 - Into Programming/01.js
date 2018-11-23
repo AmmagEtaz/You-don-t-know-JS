d95223576bd3370aec0aa9c7245b5bc8c6ea1c33
Ammagetaz <43318050+AmmagEtaz@users.noreply.github.com>
2018-11-23 04:18:22 +0100

    Create 01.js

---
+++ Up and Going/Chapter 01 - Into Programming/01.js
@@ -0,0 +1,31 @@
+const SPENDING_TRESHOLD = 300;
+const TAX_RATE = 0.08;
+var PHONE_COST = 109.99;
+var ACCESSORY_COST = 8.99;
+
+var budget = 390;
+var amount = 0;
+
+function addTax(amount) {
+    return amount * TAX_RATE;
+}
+
+function formatAmount(amount) {
+  return "$" + amount.toFixed( 2 );
+}
+
+while(amount < budget) {
+
+  amount = amount + PHONE_COST;
+
+  if(amount < SPENDING_TRESHOLD) {
+      amount = amount + ACCESSORY_COST;
+    }
+  }
+
+amount = amount + addTax( amount );
+console.log("Your purchase plus tax" + formatAmount( amount );
+
+if(amount > budget) {
+  console.log("You cannot afford this purchase");
+}
